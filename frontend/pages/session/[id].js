fbbadc1e8f9c9f13254494b7ddb480b8b6abdda8
Jeremias Osuna <jeremias.osuna@trocafone.com>
2023-09-08 15:58:39 -0300

    Fixes

--- frontend/pages/session/[id].js
+++ frontend/pages/session/[id].js
@@ -81,7 +81,7 @@ export default function Page() {
 		setShuffledMemoBlocks(sessionImages);
 	}, [winner]);
 
-	function shuffleArray(array) {
+	const shuffleArray = (array) => {
 		const shuffledArray = [...array];
 
 		function compareRandom() {
